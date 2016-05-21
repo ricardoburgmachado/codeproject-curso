990922bf5282f8668112b9f428bd477a0ddeab02
Ricardo Machado <ricardopx_burg@yahoo.com.br>
2016-05-21 15:09:39 -0300

    includes de tranformers

--- app/Transformers/ProjectTransformer.php
+++ app/Transformers/ProjectTransformer.php
@@ -14,19 +14,35 @@ use League\Fractal\TransformerAbstract;
 
 class ProjectTransformer extends TransformerAbstract {
 
+
+    //faz uma chamada do método includeMembers, usando apenas parte de seu nome, 'members'
+    //serão incluidos esses dados no array de dados construido abaixo (Project)
+    protected $defaultIncludes = ['members'];
     
     
     public function transform(Project $project){
 
         return [
             'project_id'=> $project->id,
-            'project'=> $project->name,
+            'client_id'=>$project->client_id,
+            'owner_id'=>$project->owner_id,
+            'name'=> $project->name,
             'decription'=> $project->description,
             'progress'=> $project->progress,
             'status'=> $project->status,
             'due_date'=> $project->due_date
         ];
-
     }
+
+    public function includeMembers(Project $project){
+
+        return $this->collection($project->members,
+            new ProjectMemberTransformer() );
+    }
+
+
+
+
+
 
 }
